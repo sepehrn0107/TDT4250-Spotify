0e6dc4b75c8e9a821eac3c28e062226807453840
vegarrsm <arnedeig@gmail.com>
2022-12-12 20:41:51 +0100

    Fixed fetching

---
+++ Data/fetchArtists.py
@@ -0,0 +1,8 @@
+import spotipy
+import csv
+from spotipy.oauth2 import SpotifyClientCredentials
+
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id="a087144b6ff14d22864514e26b34fe93",client_secret="3594c547263a48f38f961ee1c3a415ad"))
+
+
+
