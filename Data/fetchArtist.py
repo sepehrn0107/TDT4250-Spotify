89e248e2ffd80bdef71676742ac152a96ae767b1
vegarrsm <arnedeig@gmail.com>
2022-12-12 19:23:40 +0100

    fixed fetching to match data structure in java

--- Data/fetchArtist.py
+++ Data/fetchArtist.py
@@ -6,21 +6,35 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(cl
 
 artist = spotify.search(input("Artist name: "),1, 0, "artist")["artists"]["items"][0]
 
+def checkArtist(id:str):
+    artist = spotify.artist(id)
+    exists = False
+    with open("artistList.csv","r+",newline='',encoding="utf-8") as f:
+        reader = csv.reader(f)
+        writer = csv.writer(f)
+        for item in reader:
+            if(item and item[0]==artist["id"]):
+                exists = True
+        if not exists:
+            writer.writerow([artist["id"],artist["name"],artist["followers"]["total"],artist["genres"][0]])
+
+
 results = spotify.artist_albums(artist["uri"], album_type='album')
 albums = results['items']
+
 while results['next']:
     results = spotify.next(results)
     albums.extend(results['items'])
 
 filename = "./artist_"+"".join(x for x in artist["name"] if x.isalnum())+".csv"
 
-with open(filename,"w") as f:
+checkArtist(artist["id"])
+
+with open(filename,"w", encoding="utf-8",newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(["Track title", "Album title","Artists", "Duration (ms)","Artist IDs","Track ID", "Album ID"])
+    writer.writerow(["Track Id", "Track title","href", "Duration (ms)","Explicit","Artist IDs", "Album ID"])
     for album in albums:
         albumName = album["name"]
         tracksFromAlbum = spotify.album_tracks(album["uri"])
         for track in tracksFromAlbum["items"]:
-            artistNames = " | ".join(artist["name"] for artist in track["artists"])
-            artistIDs = " | ".join(artist["id"] for artist in track["artists"])
-            writer.writerow([track["name"],albumName,artistNames,track["duration_ms"],track["id"],album["id"],artistIDs])
+            writer.writerow([track["id"],track["name"],track["href"],track["duration_ms"],track["explicit"],track["artists"][0]["id"],album["id"]])
--- Data/fetchPlaylist.py
+++ Data/fetchPlaylist.py
@@ -6,15 +6,29 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(cl
 
 playlist = spotify.search(input("playlist name: "),1, 0, "playlist")["playlists"]["items"][0]
 
+def checkArtist(id:str):
+    artist = spotify.artist(id)
+    exists = False
+    with open("artistList.csv","r+",newline='',encoding="utf-8") as f:
+        reader = csv.reader(f)
+        writer = csv.writer(f)
+        for item in reader:
+            if(item and item[0]==artist["id"]):
+                exists = True
+        if not exists:
+            writer.writerow([artist["id"],artist["name"],artist["followers"]["total"]])
+
 
 results = spotify.playlist_tracks(playlist["uri"])
 
 filename = "./playlist_"+"".join(x for x in playlist["name"] if x.isalnum())+".csv"
 
-with open(filename,"w") as f:
+with open(filename,"w",newline='',encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(["Track title", "Album title", "duration (ms)","Artist names", "Artist IDs","track ID", "album ID"])
+    writer.writerow([playlist["owner"]["id"],playlist["id"],playlist["name"],playlist["description"],playlist["public"]])
+    writer.writerow(["track ID","Track title","href",  "duration (ms)","explicit","Artist ID", "album ID"])
     for item in results["items"]:
-        artistNames = " | ".join(artist["name"] for artist in item["track"]["artists"])
-        artistIDs = " | ".join(artist["id"] for artist in item["track"]["artists"])
-        writer.writerow([item["track"]["name"],item["track"]["album"]["name"],item["track"]["duration_ms"],artistNames,artistIDs,item["track"]["artists"][0]["id"],item["track"]["album"]["id"]])
+        for art in item["track"]["artists"]:
+            checkArtist(art["id"])
+        writer.writerow([item["track"]["id"],item["track"]["name"],item["track"]["href"],item["track"]["duration_ms"],item["track"]["explicit"],item["track"]["artists"][0]["id"],item["track"]["album"]["id"]])
+
